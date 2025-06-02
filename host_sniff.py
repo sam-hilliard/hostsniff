893f52d8ccb2b4c08749826f4834102fec684b80
Sam Hilliard <samhilliard51@gmail.com>
2025-06-01 22:21:08 -0400

    Simplified code

--- host_sniff.py
+++ host_sniff.py
@@ -1,20 +1,14 @@
 from scapy.all import sniff, ARP, IP, Ether
+
 seen_hosts = set()
 
 def packet_handler(pkt):
     mac = pkt[Ether].src if pkt.haslayer(Ether) else "N/A"
-    
-    if pkt.haslayer(ARP):
-        ip = pkt[ARP].psrc
+
+    if pkt.haslayer(ARP) or pkt.haslayer(IP):
+        ip = pkt[ARP].psrc if pkt.haslayer(ARP) else pkt[IP].src
         if ip != '0.0.0.0' and (ip, mac) not in seen_hosts:
             seen_hosts.add((ip, mac))
-            print(f"[ARP] Host discovered: {ip} | MAC: {mac}")
+            print(f"{ip}: {mac}")
 
-    elif pkt.haslayer(IP):
-        src_ip = pkt[IP].src
-        if (src_ip, mac) not in seen_hosts:
-            seen_hosts.add((src_ip, mac))
-            print(f"[IP ] Host discovered: {src_ip} | MAC: {mac}")
-
-print("sniffing...")
 sniff(filter="broadcast or multicast", prn=packet_handler, store=False)
