26593b17eb1d2f4c87fb58aa7c0a5b0112b70199
Sam Hilliard <samhilliard51@gmail.com>
2025-06-01 23:15:36 -0400

    Organized output into table

--- host_sniff.py
+++ host_sniff.py
@@ -1,14 +1,28 @@
 from scapy.all import sniff, ARP, IP, Ether
+from rich.live import Live
+from rich.table import Table
+from rich import box
 
-seen_hosts = set()
+seen_hosts = {}
+
+def build_table():
+    table =  Table(box=box.SIMPLE)
+    table.add_column("IP Address", style="cyan")
+    table.add_column("MAC Address", style="magenta")
+    for ip, mac in seen_hosts.items():
+        table.add_row(ip, mac)
+    return table
 
 def packet_handler(pkt):
     mac = pkt[Ether].src if pkt.haslayer(Ether) else "N/A"
-
     if pkt.haslayer(ARP) or pkt.haslayer(IP):
         ip = pkt[ARP].psrc if pkt.haslayer(ARP) else pkt[IP].src
-        if ip != '0.0.0.0' and (ip, mac) not in seen_hosts:
-            seen_hosts.add((ip, mac))
-            print(f"{ip}: {mac}")
+        if ip != '0.0.0.0' and ip not in seen_hosts:
+            seen_hosts[ip] = mac
 
-sniff(filter="broadcast or multicast", prn=packet_handler, store=False)
+def handle(pkt):
+    packet_handler(pkt)
+    live.update(build_table())
+
+with Live(build_table(), refresh_per_second=2) as live:
+    sniff(filter="broadcast or multicast", prn=handle, store=False)
